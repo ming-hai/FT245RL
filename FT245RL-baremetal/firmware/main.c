b0002f25cd2d5456e7b40328a9794fa76b27ee41
Arjan <Arjan.van.Vught@gmail.com>
2015-01-23 20:21:03 +0100

    Added project baremetal

---
+++ FT245RL-baremetal/firmware/main.c
@@ -0,0 +1,163 @@
+/**
+ * @file main.c
+ *
+ */
+/* Copyright (C) 2015 by Arjan van Vught <pm @ http://www.raspberrypi.org/forum/>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include "bcm2835.h"
+#include "bcm2835_gpio.h"
+
+void __attribute__((interrupt("IRQ"))) c_irq_handler(void) {}
+void __attribute__((interrupt("FIQ"))) c_fiq_handler(void) {}
+
+// TODO Place the jumper on the left side for 3V3
+/*
+ * Raspberry Pi
+ * P5:Broadcom			FT245RL
+ *
+ *  3:GPIO02	<--->	D0
+ *  5:GPIO03	<--->	D1
+ *  7:GPIO04	<--->	D2
+ * 26:GPIO07	<--->	D3
+ * 24:GPIO08	<--->	D4
+ * 21:GPIO09	<--->	D5
+ * 19:GPIO10	<--->	D6
+ * 23:GPIO11	<--->	D7
+ *
+ * 15:GPIO22	---->	WR
+ * 16:GPIO23	---->	RD#
+ *
+ * 18:GPIO24	<----	TXE#
+ * 22:GPIO25	<----	RXF#
+ */
+
+#define WR	22
+#define _RD	23
+
+void data_gpio_fsel_output() {
+	// Data output
+	uint32_t value = BCM2835_GPIO->GPFSEL0;
+	value &= ~(7 << 6);
+	value |= BCM2835_GPIO_FSEL_OUTP << 6;
+	value &= ~(7 << 9);
+	value |= BCM2835_GPIO_FSEL_OUTP << 9;
+	value &=  ~(7 << 12);
+	value |= BCM2835_GPIO_FSEL_OUTP << 12;
+	value &= ~(7 << 21);
+	value |= BCM2835_GPIO_FSEL_OUTP << 21;
+	value &= ~(7 << 24);
+	value |= BCM2835_GPIO_FSEL_OUTP << 24 ;
+	value &= ~(7 << 27);
+	value |= BCM2835_GPIO_FSEL_OUTP << 27;
+	BCM2835_GPIO->GPFSEL0 = value;
+	value = BCM2835_GPIO->GPFSEL1;
+	value &= ~(7 << 0) ;
+	value |= BCM2835_GPIO_FSEL_OUTP << 0 ;
+	value &= ~(7 << 3);
+	value |= BCM2835_GPIO_FSEL_OUTP << 3;
+	BCM2835_GPIO->GPFSEL1 = value;
+}
+
+void data_gpio_fsel_input() {
+	// Data input
+	uint32_t value = BCM2835_GPIO->GPFSEL0;
+	value &= ~(7 << 6);
+	value |= BCM2835_GPIO_FSEL_INPT << 6;
+	value &= ~(7 << 9);
+	value |= BCM2835_GPIO_FSEL_INPT << 9;
+	value &=  ~(7 << 12);
+	value |= BCM2835_GPIO_FSEL_INPT << 12;
+	value &= ~(7 << 21);
+	value |= BCM2835_GPIO_FSEL_INPT << 21;
+	value &= ~(7 << 24);
+	value |= BCM2835_GPIO_FSEL_INPT << 24 ;
+	value &= ~(7 << 27);
+	value |= BCM2835_GPIO_FSEL_INPT << 27;
+	BCM2835_GPIO->GPFSEL0 = value;
+	value = BCM2835_GPIO->GPFSEL1;
+	value &= ~(7 << 0) ;
+	value |= BCM2835_GPIO_FSEL_INPT << 0 ;
+	value &= ~(7 << 3);
+	value |= BCM2835_GPIO_FSEL_INPT << 3;
+	BCM2835_GPIO->GPFSEL1 = value;
+}
+
+void FT245RL_init(void) {
+	// RD#, WR output
+	uint32_t value = BCM2835_GPIO->GPFSEL2;
+	value &= ~(7 << 6) | ~(7 << 9);
+	value |= BCM2835_GPIO_FSEL_OUTP << 6 | BCM2835_GPIO_FSEL_OUTP << 9;
+	// TXE#, RXF# input
+	value &= ~(7 << 12) | ~(7 << 15);
+	value |= BCM2835_GPIO_FSEL_INPT << 12 | BCM2835_GPIO_FSEL_INPT << 15;
+	BCM2835_GPIO->GPFSEL2 = value;
+	// RD#	high
+	bcm2835_gpio_set(_RD);
+	// WR	low
+	bcm2835_gpio_clr(WR);
+}
+
+void FT245RL_write_data(uint8_t data) {
+	data_gpio_fsel_output();
+	// Raise WR to start the write.
+	bcm2835_gpio_set(WR);
+	asm volatile("nop"::);
+	// Put the data on the bus.
+	uint32_t out_gpio = ((data & ~0b00000111) << 4) | ((data & 0b00000111) << 2);
+	BCM2835_GPIO->GPSET0 = out_gpio;
+	BCM2835_GPIO->GPCLR0 = out_gpio ^ 0b111110011100;
+	asm volatile("nop"::);
+	// Drop WR to tell the FT245 to read the data.
+	bcm2835_gpio_clr(WR);
+}
+
+uint8_t FT245RL_read_data() {
+	data_gpio_fsel_input();
+	bcm2835_gpio_clr(_RD);
+	// Wait for the FT245 to respond with data.
+	asm volatile("nop"::);
+	// Read the data from the data port.
+	uint32_t in_gpio = (BCM2835_GPIO->GPLEV0 & 0b111110011100) >> 2;
+	uint8_t data = (uint8_t)((in_gpio >> 2) & 0xF8) | (uint8_t)(in_gpio & 0x0F);
+	// Bring RD# back up so the FT245 can let go of the data.
+	bcm2835_gpio_set(_RD);
+	// Wait to prevent false 'no data' readings.
+	asm volatile("nop"::);
+	return data;
+}
+
+int notmain(int argc, char *argv[]) {
+	FT245RL_init();
+
+	while (1) {
+		if (!(BCM2835_GPIO->GPLEV0 & (1 << 25))) {
+			uint8_t data = FT245RL_read_data();
+			if (data == '\r')
+				FT245RL_write_data('\n');
+			FT245RL_write_data(data);
+		}
+	}
+
+	return 0;
+}
---
+++ FT245RL-baremetal/include/pro_driver.h
@@ -0,0 +1,32 @@
+#ifndef _PRO_DRIVER_H
+#define _PRO_DRIVER_H
+
+
+#define GET_WIDGET_PARAMS 3
+#define GET_WIDGET_SN 10
+#define GET_WIDGET_PARAMS_REPLY 3
+#define SET_WIDGET_PARAMS 4
+#define SET_DMX_RX_MODE 5
+#define SET_DMX_TX_MODE 6
+#define SEND_DMX_RDM_TX 7
+#define RECEIVE_DMX_ON_CHANGE 8
+#define RECEIVED_DMX_COS_TYPE 9
+#define ONE_BYTE 1
+#define DMX_START_CODE 0x7E 
+#define DMX_END_CODE 0xE7 
+#define OFFSET 0xFF
+#define DMX_HEADER_LENGTH 4
+#define BYTE_LENGTH 8
+#define HEADER_RDM_LABEL 5
+#define NO_RESPONSE 0
+#define DMX_PACKET_SIZE 512
+
+
+#define MAX_PROS 20
+#define SEND_NOW 0
+#define TRUE 1
+#define FALSE 0
+#define HEAD 0
+#define IO_ERROR 9
+
+#endif
